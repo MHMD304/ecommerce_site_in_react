5f62673f640242b0d6f27475ee6e8f5192e81bcd
MHMD304 <slymanmhamad86@gmail.com>
2025-09-13 02:27:46 +0300

    Add routes to pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,18 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import ProductDetails from './components/ProductDetails.jsx'
+import Basket from './components/Basket.jsx'
+import Checkout from './components/Checkout.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+        <Routes>
+            <Route path='/' element={<App />}/>
+            <Route path='products/:pid' element={<ProductDetails/>}  />
+            <Route path='basket' element={<Basket/>}/>
+            <Route path='checkout' element={<Checkout/>}/>
+        </Routes>
+        
+    </BrowserRouter>
 )
